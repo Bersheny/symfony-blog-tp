7a28d6a181b5d8bfe22e8279ef73d8b682cc8fdf
Bersheny <fhezrghjsd4@live.fr>
2023-03-29 13:42:38 +0200

    1

--- src/Form/Formation1Type.php
+++ src/Form/Formation1Type.php
@@ -4,8 +4,11 @@ namespace App\Form;
 
 use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class Formation1Type extends AbstractType
 {
@@ -13,13 +16,21 @@ class Formation1Type extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('content')
+            ->add('content', CKEditorType::class)
             ->add('capacity')
             ->add('price')
-            ->add('createdAt')
+            // ->add('createdAt')
             ->add('description')
-            ->add('startDateTime')
-            ->add('endDateTime')
+            ->add('startDateTime',  DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'input' => 'datetime_immutable',
+            ])
+            ->add('endDateTime', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'input' => 'datetime_immutable',
+            ])
             ->add('speaker')
             ->add('createdBy')
         ;
